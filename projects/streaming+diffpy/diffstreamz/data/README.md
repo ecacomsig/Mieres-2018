cfd8c6e2faddbad81cf26a86b326b3b922c2db48
Duncan Johnstone <duncanjohnstone@live.co.uk>
2018-08-20 15:47:48 +0200

    document data creation and provide script

---
+++ projects/streaming+diffpy/diffstreamz/data/README.md
@@ -0,0 +1,5 @@
+Datasets here produced using /diffstreamz/generate_data.py
+
+The data comprizes two Gaussian functions  with Gaussian noise added to the data points. The two Gaussian functions move smoothly through a series of 100 'spectra' in 'peaks_moving_smoothly'.
+
+These data can be loaded using np.loadtxt('peaks_moving_smoothly', delimiter=',')
---
+++ projects/streaming+diffpy/diffstreamz/diffstreamz/generate_data.py
@@ -0,0 +1,66 @@
+from scipy.ndimage import gaussian_filter
+import numpy as np
+import hyperspy.api as hs
+# set the parameters:
+blurs = [0., 1.]
+radius = 3  #radius of a different region
+domain = 10 #size of the square domain
+small_centres = (500, 900)
+small_amplitudes = (8000,  5000)
+
+# work:
+total = None
+cent = (domain//2, domain//2)
+y,x = np.ogrid[-cent[0]:domain-cent[0], -cent[1]:domain-cent[1]]
+mask = x*x + y*y <= radius*radius
+for blur in blurs:
+
+    s = hs.signals.Signal1D(np.ones((domain,domain, 1024)))
+    cent = tuple([int(0.5*i) for i in s.data.shape[:-1]])
+    m0 = s.create_model()
+    gs01 = hs.model.components1D.Gaussian()
+    m0.append(gs01)
+    gs01.sigma.value = 300
+    gs01.centre.map['values'][:] = (np.random.random((domain,domain)) - 0.5)*50 + small_centres[0]
+    gs01.centre.map['is_set'][:] = True
+    gs01.A.map['values'][:] = 1000 * np.random.random((domain,domain)) + 300000
+    gs01.A.map['is_set'][:] = True
+
+    gs02 = hs.model.components1D.Gaussian()
+    m0.append(gs02)
+    gs02.sigma.value = 30
+    gs02.centre.map['values'][:] = (np.random.random((domain,domain)) - 0.5)*50 +  small_centres[1]
+
+    gs02.centre.map['values'][mask] = (np.random.random(gs02.centre.map['values'][mask].shape) - 0.5)*50 + 700
+    #gs02.centre.map['values'][10:20,10:20] = (np.random.random((10,10)) - 0.5)*100 + 200
+
+    gs02.centre.map['values'] = gaussian_filter(gs02.centre.map['values'], blur)
+    gs02.centre.map['is_set'][:] = True
+    gs02.A.map['values'][:] = small_amplitudes[0]
+    gs02.A.map['values'][mask] = small_amplitudes[1]
+    gs02.A.map['values'] = gaussian_filter(gs02.A.map['values'], blur)
+    gs02.A.map['is_set'][:] = True
+    s11 = m0.as_signal()
+    if total is None:
+        total = s11.data.copy()
+    else:
+        total = np.concatenate((total, s11.data), axis=1)
+        
+s = hs.signals.Signal1D(total)
+s.metadata.General.title = 'Two gaussians'
+
+s.add_poissonian_noise()
+s.estimate_poissonian_noise_variance()
+s.axes_manager[0].name = "x"
+s.axes_manager[0].units = "nm"
+s.axes_manager[1].name = "y"
+s.axes_manager[1].units = "nm"
+
+s.axes_manager[2].name = "Energy"
+s.axes_manager[2].name = "eV"
+s.axes_manager[2].scale = 0.1
+
+a = s.inav[5,5].data
+np.savetxt(X=a, fname="two_peaks", delimiter=',')
+b = s.inav[10:20,:].data
+np.savetxt(X=a, fname="peaks_moving_smoothly", delimiter=',')
