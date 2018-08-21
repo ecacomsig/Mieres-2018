28c36d25e3f72779700ecd9da115d1e9907f6c59
Felix Simkovic <felixsimkovic@me.com>
2018-08-21 18:16:01 +0200

    bug fix

--- projects/pysftools/pysftools/data.py
+++ projects/pysftools/pysftools/data.py
@@ -14,86 +14,42 @@ class ReflectionData(object):
     def __init__(self, reflection_file):
         self.reflection_file = reflection_file
         self.reflection_data = reflection_file_reader.any_reflection_file(file_name=reflection_file)
-        self.miller_arrays = self.reflection_data.as_miller_arrays()
-        
+
     def get(self, labels):
-        """
-
-        Parameters
-        ----------
-        labels : list
-            Column labels for the miller array you wish to return
-
-        Raises
-        ------
-        :exc: `RuntimeError`
-
-        """
         for m_a in self.miller_arrays:
             if m_a.info().labels == labels:
                 return m_a
-            else:
-                raise RuntimeError("{0} columns not found in {1}".format(labels, self.reflection_file))
+        raise RuntimeError("{} columns not found in {}".format(labels, self.reflection_file))
 
     def i2f(self, labels):
-        """
-
-        Parameters
-        ----------
-        labels : list
-            Column labels for the intensities you wish to convert e.g. ['I', 'SIGI']
-
-        Returns
-        -------
-
-        """
-
         m_a = self.get(labels)
-
-        assert m_a.is_intensity_array()
-
+        if not m_a.is_intensity_array():
+            raise ValueError('Array is not an intensity array')
         array_info = miller.array_info()
         array_info.labels = ['Fobs', 'Sigma-Fobs']
-        amplitude_m_a = m_a.customized_copy(observation_type=observation_types.amplitude(),
-                                            info=array_info)
-
+        amplitude_m_a = m_a.customized_copy(observation_type=observation_types.amplitude(), info=array_info)
         return amplitude_m_a
 
     def reindex(self, sg):
-        """
-        Function to reindex space group
-
-        :param sg:
-        :return:
-        """
         for m_a in self.miller_arrays:
             if not looks_like_r_free_flags_info(m_a.info()):
                 m_a.change_symmetry(sg)
 
     def checkhkl(self):
-        """
-
-        :return:
-        """
         def unique_reflections(m_a):
             indices = m_a.indices()
             return len({tuple(r) for r in indices.as_vec3_double()}) == indices.size()
-        
+
         # return all(unique_reflections(m_a) for m_a in self.miller_arrays)
 
         self.miller_arrays[0].show_comprehensive_summary()
 
         self.miller_arrays[0].analyze_intensity_statistics().show()
 
-
     @property
     def labels(self):
         return [m_a.info().labels for m_a in self.miller_arrays]
 
-
-if __name__ == '__main__':
-    import sys
-
-    rd = ReflectionData(sys.argv[1])
-    rd.checkhkl()
-
+    @property
+    def miller_arrays(self):
+        return self.reflection_data.as_miller_arrays()
